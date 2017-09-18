d7f1ff995d3ccf498e6d317902156dfeeecbf4dc
Jaren831 <jaren.lynchmobb@gmail.com>
2017-09-18 01:47:54 -0700

    More PEP8 changes

--- backbone.py
+++ backbone.py
@@ -2,19 +2,22 @@ import fresh_tomatoes
 import movie
 
 # Drive
-drive = movie.Movie("Drive",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Drive2011Poster.jpg",  # NOQA
-                        "https://www.youtube.com/watch?v=CWX34ShfcsE")  # NOQA
+drive = movie.Movie(
+    "Drive",
+    "https://upload.wikimedia.org/wikipedia/en/1/13/Drive2011Poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=CWX34ShfcsE")  # NOQA
 
 # Battleship
-battleship = movie.Movie("Battleship",
-                        "https://upload.wikimedia.org/wikipedia/en/6/6e/Battleship_Poster.jpg",  # NOQA
-                        "https://www.youtube.com/watch?v=qcmYSxnYZV4")  # NOQA
+battleship = movie.Movie(
+    "Battleship",
+    "https://upload.wikimedia.org/wikipedia/en/6/6e/Battleship_Poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=qcmYSxnYZV4")  # NOQA
 
 # The Matrix
-the_matrix = movie.Movie("The Matrix",
-                        "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",  # NOQA
-                        "https://www.youtube.com/watch?v=m8e-FF8MsqU")  # NOQA
+the_matrix = movie.Movie(
+    "The Matrix",
+    "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=m8e-FF8MsqU")  # NOQA
 
 # List of movies
 movies = [drive, battleship, the_matrix]
